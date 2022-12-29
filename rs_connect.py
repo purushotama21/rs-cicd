51a5ccebc94beedf5277de7ec4030f514eea9d66
purushotama21 <70282321+purushotama21@users.noreply.github.com>
2022-12-29 13:11:23 +0530

    Update rs_connect.py

--- rs_connect.py
+++ rs_connect.py
@@ -2,7 +2,7 @@ import psycopg2
 conn = psycopg2.connect(
     host="redshift-cluster-2.cn8ui4uqu7fc.us-west-1.redshift.amazonaws.com",
     database="dev",
-    user="awsuser2",
+    user="awsuser",
     port=5439,
     password="Awsuser123")
 cur = conn.cursor()
