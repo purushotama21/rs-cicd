dd898e5f5de3c0c87ba25bd94ee5ce6f35c10c77
purushotama21 <70282321+purushotama21@users.noreply.github.com>
2022-12-29 12:48:40 +0530

    updated the rs.py

--- rs_connect.py
+++ rs_connect.py
@@ -1,15 +1,17 @@
 import psycopg2
-def handler(event,context):
-    conn = psycopg2.connect(
-        host="redshift-cluster-1.cn8ui4uqu7fc.us-west-1.redshift.amazonaws.com",
-        database="dev",
-        user="admin",
-        port=5439,
-        password="Admin123")
-    cur = conn.cursor()
-    print("connection succussfully established")
-    cur.execute("select * from users")
-    print(cur.fetchall())
-    print(".........")
-    print()
-    conn.close()
+conn = psycopg2.connect(
+    host="redshift-cluster-1.cn8ui4uqu7fc.us-west-1.redshift.amazonaws.com",
+    database="dev",
+    user="admin",
+    port=5439,
+    password="Admin123")
+cur = conn.cursor()
+with open('emp.sql') as f:
+    cursor.execute(f.read().decode('utf-8'), multi=True)
+#sqlfile=open("emp.sql","r")
+#cur.execute("sqlfile")
+print("connection succussfully established")
+cur.execute("select * from users")
+print(cur.fetchall())
+print(".........")
+print()
